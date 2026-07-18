6e95b0cfe9e63a0f3a6e27f1a3525d7a00076e1e
tapirCode <muhamadfikri8798@gmail.com>
2026-07-18 12:22:12 +0800

    update

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 const menuButton = document.querySelector(".menu-button")
 const navbarSection = document.querySelector(".navbar")
 const sectionHeader = document.querySelector(".section-header")
+const blurBody = document.querySelector('.blur-body')
+const darkMode = document.querySelector('.dark-mode')
+
+darkMode.addEventListener('click', () =>{
+    alert('fitur mode dark mode, dalam proses')
+    })
+
 menuButton.addEventListener("click",()=>{
     menuButton.classList.toggle("active")
 navbarSection.classList.toggle("aktif")
+blurBody.classList.toggle("active-blur")
+document.body.classList.toggle("body-mode")
 })
 
 window.addEventListener("click",(e)=>{
 if (!sectionHeader.contains(e.target)) {
     menuButton.classList.remove("active")
     navbarSection.classList.remove("aktif")
+    blurBody.classList.remove("active-blur")
+    
 }
 })
