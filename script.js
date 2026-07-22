b1677aecd5da4c5950ed965e8f0af41ce34f2ffa
tapirCode <muhamadfikri8798@gmail.com>
2026-07-22 11:39:30 +0800

    update

--- script.js
+++ script.js
@@ -3,23 +3,23 @@ const navbarSection = document.querySelector(".navbar")
 const sectionHeader = document.querySelector(".section-header")
 const blurBody = document.querySelector('.blur-body')
 const darkMode = document.querySelector('.dark-mode')
-
+//fungsi untuk button mode dark mode
 darkMode.addEventListener('click', () =>{
     alert('fitur mode dark mode, dalam proses')
     })
-
+//fungsi untuk menambahkan/memasang class 
 menuButton.addEventListener("click",()=>{
     menuButton.classList.toggle("active")
 navbarSection.classList.toggle("aktif")
 blurBody.classList.toggle("active-blur")
 document.body.classList.toggle("body-mode")
 })
-
+//fungsi untuk menghapus class
 window.addEventListener("click",(e)=>{
 if (!sectionHeader.contains(e.target)) {
     menuButton.classList.remove("active")
     navbarSection.classList.remove("aktif")
     blurBody.classList.remove("active-blur")
-    
+    document.body.classList.remove("body-mode")
 }
 })
